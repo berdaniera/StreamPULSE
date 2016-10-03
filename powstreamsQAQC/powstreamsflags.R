bd59158e4d242db8e1150bf40749f430686147c6
Aaron Berdanier <aaron.berdanier@gmail.com>
2016-10-03 15:08:42 -0400

    Speed up time slicing

--- powstreamsQAQC/powstreamsflags.R
+++ powstreamsQAQC/powstreamsflags.R
@@ -3,30 +3,47 @@ library(lubridate)
 library(powstreams)
 library(sbtools)
 library(tibble)
-login_sb()
+login_sb('abb30@duke.edu')
 
 #######################################3
-getdtwin <- function(x, mins){
-  # get even date-time windows
-  # x is a vector of date times
-  # mins is the minutes per window
-  xx <- as.POSIXct(date(x))
-  rng <- seq(min(xx), max(xx), by=mins*60)
-  list(dts=as.POSIXct(cut(x,rng)), brks=rng)
+# Snap time stamps to even interval
+snap = function(x, freq, nearest=FALSE){
+  # x is a date time object to be snapped
+  # freq is the frequency of observations
+  # nearest is logical to snap to floor (default) or to nearest time cut
+  re = regexec("([0-9]+)([A-Z])",freq)[[1]]
+  if(-1%in%re){
+    stop("Please enter a correct string")
+  }else{
+    ml = attr(re,"match.length")
+    nn = as.numeric(substr(freq, re[2], ml[2]))
+    uu = substr(freq, re[3], ml[1])
+    if(uu=="D"){td = 24*60*60*nn
+    }else if(uu=="H"){td = 60*60*nn
+    }else if(uu=="M"){td = 60*nn
+    }else if(uu=="S"){td = nn
+    }else{stop("Please enter a correct string")}
+  }
+  if(nearest){ # round to closest interval
+    as.POSIXct(round(as.double(x)/td)*td,origin="1970-01-01")
+  }else{ # round to floor
+    as.POSIXct(floor(as.double(x)/td)*td,origin="1970-01-01")
+  }
 }
 
 getccf <- function(tsdf, xvar, yvar, window, plt=TRUE){
-  # Check for differences in time windows - need to re-window for cross correlation
-  # NOTE: Slow! Also, it might be overkill...
-  if( (length(which(diff(tsdf$DateTime)==window))/nrow(tsdf) < 0.9) ){ # 90% of time windows are 15 minutes
-  #if(all( diff(ts$DateTime) != 0 )){ # all time windows are 15 minutes - more restrictive
-    windows <- getdtwin(tsdf$DateTime, window)
-    tsdf$DateTime <- windows$dts
-    # select data columns, average by time windows (in case some duplicates)
-    tsdf <- tsdf %>% group_by(DateTime) %>% summarise_each(funs(mean))
-    # merge with date time windows
-    tsdf <- left_join(data.frame(DateTime=windows$brks), tsdf, by="DateTime")
+  # snap times to 15M intervals
+  windows <- snap(tsdf$DateTime,"15M")
+  if(any(duplicated(tsdf$DateTime))){
+    # cut out duplicate dates - only one value per timestamp
+    tsdf <- tsdf %>% slice(match(DateTime,unique(DateTime))) %>% arrange(DateTime)
+    # Alternative: average by datetime
+    #tsdf <- tsdf %>% group_by(DateTime) %>% summarise_each(funs(mean)) %>% ungroup()
   }
+  drng <- range(as.Date(windows))
+  cuts <- seq(min(windows),max(windows),by=window*60)
+  tsdf <- left_join(data.frame(DateTime=cuts), tsdf, by="DateTime")
+
   xv <- tsdf %>% select_(.dots=xvar)
   yv <- tsdf %>% select_(.dots=yvar)
   ccf(xv, yv, na.action=na.pass, plot=plt, lag.max=12*60/window, type="correlation") # maximum lag is 12 hours either direction
