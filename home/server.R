dc332c68b5b48c5a08d51334577fb3039c31241c
Aaron Berdanier <aaron.berdanier@gmail.com>
2016-11-04 16:48:39 -0400

    asdf

--- home/server.R
+++ home/server.R
@@ -30,6 +30,7 @@ useSB = TRUE
 if(!useSB){
   s3load('meta/streampulseusers.Rda',bucket='streampulse') # load user dataset
 }
+
 # drop_get(path="streampulseusers.Rdata",local_file=file.path(tempdir(),"spusers.Rdata"),dtoken=dto,overwrite=TRUE) # getting rda file
 # load(file.path(tempdir(),"spusers.Rdata")) # load the Rda file
 
